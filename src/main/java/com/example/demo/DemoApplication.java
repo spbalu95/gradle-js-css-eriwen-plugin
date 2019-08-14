b56bddd4e0129455ef0026a6c9c5d61ba1328757
bala <sp.balu95@gmail.com>
2019-08-14 13:39:05 +0530

    main changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		System.out.println("DemoApplication!!!");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
