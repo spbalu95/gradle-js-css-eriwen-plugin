16a9378d118829b9e88e4f4a7cbfd0fce6bb68df
bala <sp.balu95@gmail.com>
2019-08-14 13:49:03 +0530

    css file changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args)
 	{
 		System.out.println("DemoApplication!!!");
+		System.out.println("DemoApplication!!!");
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
